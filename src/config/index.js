b0fe50317f6bef0a0048f561fad76d12b4a58628
bedrosenator <bedrosenator@gmail.com>
2020-05-26 10:13:28 +0300

    api config

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+export const config = {
+  api: {
+    url: {
+      locationSearch: 'https://www.metaweather.com/api/location/search/?query=',
+      location: 'https://www.metaweather.com/api/location/',
+    }
+  }
+};
