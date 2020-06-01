3c7baf58465e773e8c5222e846d4313d12011cad
bedrosenator <bedrosenator@gmail.com>
2020-06-01 13:39:05 +0300

    added sagas to get forecast by geoposition and location

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,10 @@ export const config = {
     url: {
       locationSearch: 'https://www.metaweather.com/api/location/search/?query=',
       location: 'https://www.metaweather.com/api/location/',
-    }
+      icons: 'http://openweathermap.org/img/wn/',
+      flags: 'http://openweathermap.org/images/flags/',
+      weather: 'https://api.openweathermap.org/data/2.5/',
+    },
+    APPID: 'ec6505e64f704b64ebb5c213c1a51bbe',
   }
 };
--- src/containers/Forecast/sagas.js
+++ src/containers/Forecast/sagas.js
@@ -1,16 +1,70 @@
-import { takeLatest, put, call, select, all } from 'redux-saga/effects';
+import { takeLatest, put, call, select } from 'redux-saga/effects';
 import { config } from 'config';
+import { GET_GEO_POSITION, GET_LOCATION } from 'containers/Forecast/constants';
+import {
+  getForecastByLocationSuccess,
+  getGeoPositionSuccess,
+  getForecastByGeoPositionSuccess,
+  getForecastError,
+} from './actions';
 
 export function* sagaWatcher() {
-  yield takeLatest(['GET_FORECAST'], sagaWorker);
+  yield takeLatest([GET_GEO_POSITION], getGeoPositionSaga);
+  yield takeLatest([GET_LOCATION], sagaWorker);
+}
+
+function* makeRequest(params) {
+  const url = config.api.url.weather + 'weather?APPID=' + config.api.APPID;
+  const response = yield fetch(url + params);
+
+  if (response.status !== 200) {
+    try {
+      const json = yield response.json();
+      yield put(getForecastError({ error: json }))
+    } catch (e) {
+      yield put(getForecastError({ error: response }))
+      console.error(e);
+    }
+
+    throw new Error(response);
+  }
+
+  return yield response.json();
 }
 
 function* sagaWorker() {
-  yield call(getLocationtSaga);
+  const store = (state) => state.forecast;
+  const storeData = yield select(store);
+  const { data } = storeData;
+  try {
+    const forecast = yield call(getForecastByCitySaga, data.location);
+    yield put(getForecastByLocationSuccess(forecast));
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-async function getLocationtSaga(location) {
-  const response = await fetch(`${config.api.url.locationSearch}${location}`);
+function* getGeoPositionSaga() {
+  try {
+    const position = yield getPosition();
+    const response = yield put(getGeoPositionSuccess(position));
+    const weatherDetailsSelector = yield call(getForecastByGeoPositionSaga, response.data.coords);
+    yield put(getForecastByGeoPositionSuccess(weatherDetailsSelector));
+  } catch (e) {
+    console.error(e);
+  }
+}
 
-  return await response.json();
+function getPosition(options) {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject, options);
+  });
+}
+
+function* getForecastByGeoPositionSaga({ latitude, longitude }) {
+  return yield call(makeRequest, '&lat=' + latitude + '&lon=' + longitude );
+}
+
+function* getForecastByCitySaga(city) {
+  return yield call(makeRequest, '&q=' + city);
 }
