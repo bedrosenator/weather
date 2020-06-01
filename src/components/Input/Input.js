b7c71eeb7d28f066c944f865d512542d2ba45b42
bedrosenator <bedrosenator@gmail.com>
2020-06-01 13:37:47 +0300

    Input component refactoring

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,26 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Input.module.scss';
 
-export function Input(props) {
-  const { onChange } = props;
-  const [inputValue, setInputValue] = useState('');
+class Input extends React.Component {
+  handleChange = (event) => {
+    this.props.onChange(event.target.value);
+  }
 
-  const handleOnChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  useEffect(() => {
-    onChange(inputValue);
-  })
-
-  return (
-    <input className={styles.input} value={inputValue} onChange={handleOnChange} />
-  );
+  render() {
+    return (
+      <input className={styles.input} type="text" defaultValue={this.props.value} onChange={this.handleChange} />
+    );
+  }
 }
 
 Input.propTypes = {
   value: PropTypes.string,
+  initialValue: PropTypes.string,
   onChange: PropTypes.func.isRequired,
 };
 
