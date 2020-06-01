b223a7f310118d116d9412cccf0f9aebe1df8558
bedrosenator <bedrosenator@gmail.com>
2020-06-01 13:40:18 +0300

    root component refactoring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import Forecast from './containers/Forecast/Forecast';
+import Forecast from './containers/Forecast';
 
 function App() {
   return (
--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import forecast from 'containers/Forecast/reducer';
 
-const initialState = {};
-
 export default combineReducers({
   forecast,
 })
