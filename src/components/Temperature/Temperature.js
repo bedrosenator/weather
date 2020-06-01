2d3eb1dadcea15104cd81acc047931d20a1da4a1
bedrosenator <bedrosenator@gmail.com>
2020-06-01 13:31:38 +0300

    Temperature component added

--- src/components/Temperature/Temperature.js
+++ src/components/Temperature/Temperature.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import styles from './Temperature.module.scss';
 
 function Temperature(props) {
-  const { value, unit } = props;
+  const { value, unit, badge } = props;
+
+  const convertFromKelvinToCelsius = (temp) => {
+    return Math.round(temp - 273.15);
+  }
 
   return (
-    <div className={styles.temperature}>
-      <div>{value}°</div>
-      <div>{unit}</div>
+    <div className={classnames(styles.temperature)}>
+      <div className={classnames({ [styles.badge]: badge })}>
+        <span>{convertFromKelvinToCelsius(value)} °</span>
+        <span>{unit}</span>
+      </div>
     </div>
   );
 }
 
+Temperature.defaultProps = {
+  unit: 'C'
+}
+
 Temperature.propTypes = {
-  value: PropTypes.string,
+  value: PropTypes.number,
   unit: PropTypes.string,
+  badge: PropTypes.bool,
 };
 
 export default Temperature;
