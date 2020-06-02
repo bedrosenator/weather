a2d2d4f2acec6cb54a75441031bdb9f4f1c481dd
bedrosenator <bedrosenator@gmail.com>
2020-06-02 15:29:54 +0300

    App wrapped into ForecastBackground

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import Header from './components/Header';
+import Header from 'components/Header';
 import './App.css';
-import Forecast from './containers/Forecast';
+import Forecast from 'containers/Forecast';
+import ForecastBackground from 'containers/ForecastBackground';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <section>
-        <Forecast />
-      </section>
+      <ForecastBackground>
+        <Header />
+        <section>
+          <Forecast />
+        </section>
+      </ForecastBackground>
     </div>
   );
 }
